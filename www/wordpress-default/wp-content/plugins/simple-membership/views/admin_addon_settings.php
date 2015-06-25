bf3fcaeb5847255fd209a3918f1004ec6629949a
Justin Southworth <justin_southworth@taylor.edu>
2015-06-25 16:18:29 -0400

    Adding Simple Membership, SM-MailChimp, and SM-Login Redirection.

---
+++ www/wordpress-default/wp-content/plugins/simple-membership/views/admin_addon_settings.php
@@ -0,0 +1,14 @@
+<?php screen_icon( 'options-general' );?>
+<h1><?php echo  SwpmUtils::_('Simple WP Membership::Settings')?></h1>
+<div class="wrap">
+<?php do_action("swpm-draw-tab"); ?>
+    
+    <p>
+        <?php echo  SwpmUtils::_("Some of the simple membership plugin's addon settings and options will be displayed here (if you have them)")?>
+    </p>
+    <form action="" method="POST">
+        <input type="hidden" name="tab" value="<?php echo $current_tab;?>" />
+        <?php do_action('swpm_addon_settings_section');?>
+        <?php submit_button('Save Changes', 'primary', 'swpm-addon-settings'); ?>
+    </form>
+</div><!-- end of wrap -->
